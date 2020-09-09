7dc4cd41853e7b55c4796107653643737aeabca7
Pagwin <Pagwin.Fedora@protonmail.com>
2020-09-09 00:51:54 -0400

    added a README, a comment and changed the lang in the config

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <aspell.h>
 int main(){
+    //Setting up some configuration
     AspellConfig *spell_config = new_aspell_config();
+    aspell_config_replace(spell_config,"lang", "en_US");
 
 }
