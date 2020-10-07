4aee79752e4138416cc7f24ec1c69632820ca431
Pagwin <Pagwin.Fedora@protonmail.com>
2020-10-07 17:53:29 -0400

    added a signal handler for no reason

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <aspell.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
 int main(int argc, char* argv[]){
+    //setup signal handler
+    signal(SIGINT,sighandler);
+
     //Setting up some configuration
     AspellConfig *spell_config = new_aspell_config();
     aspell_config_replace(spell_config, "lang", "en_US");
@@ -14,4 +19,9 @@ int main(int argc, char* argv[]){
     else
 	spell_checker = to_aspell_speller(possible_err);
 
+
+}
+static void sighandler(int signal){
+    //lol no reason to do anything special with any signals but I felt like adding this anyways
+    if(signal == SIGINT) exit();
 }
