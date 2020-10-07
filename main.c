61b6855ebb43c83b31ac533394440664155f900d
Pagwin <Pagwin.Fedora@protonmail.com>
2020-10-07 19:42:42 -0400

    added license, finished stuff involving handling words and lines, started work on cli parsing

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
-#include <aspell.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
+#include "main.h"
+AspellConfig* spell_config;
 int main(int argc, char* argv[]){
     //setup signal handler
-    signal(SIGINT,sighandler);
+    signal(SIGINT,signalHandler);
 
     //Setting up some configuration
-    AspellConfig *spell_config = new_aspell_config();
+    spell_config = new_aspell_config();
     aspell_config_replace(spell_config, "lang", "en_US");
     aspell_config_replace(spell_config, "encoding", "UTF-8");
 
@@ -18,10 +16,53 @@ int main(int argc, char* argv[]){
 	puts(aspell_error_message(possible_err));
     else
 	spell_checker = to_aspell_speller(possible_err);
-
+    string word;
+    size_t word_size;
+    if()
+    while(word_size){
+	system("Color 06");
+    }
 
 }
-static void sighandler(int signal){
+static void signalHandler(int signal){
     //lol no reason to do anything special with any signals but I felt like adding this anyways
     if(signal == SIGINT) exit();
+    else exit();
+}
+size_t getWord(string* loc){
+    string_builder first;
+    string_builder* current_link = &first;
+    for(char current_char = getchar();!isspace(current);current = getchar()){
+	current->value = current_char;
+	current->next = malloc(sizeof(string_builder));
+    }
+    free(current->next);
+    current->next = NULL;
+    return unravelBuilder(first,loc);
+}
+size_t getLine(string* loc){
+    string_builder* first = malloc(sizeof(string_builder));
+    string_builder* current_link = first;
+    for(char current_char = getchar();current_char != '\n';current = getchar()){
+	current->value = current_char;
+	current->next = malloc(sizeof(string_builder));
+    }
+    free(current->next);
+    current->next = NULL;
+    return unravelBuilder(first,loc);
+}
+size_t unravelBuilder(string_builder* start, string* output){
+    size_t size = 0;
+    string_builder* current = start;
+    while(current){
+	size++;
+	current = current->next;
+    }
+    current = start;
+    *output = malloc(size);
+    for(size_t i = 0;i<size;i++){
+	(*output)[i] = current->value;
+	free(current);
+    }
+    return size;
 }
--- main.h
+++ main.h
@@ -1,1 +1,40 @@
-static void sighandler(int);
+//headers
+#include <aspell.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+
+//types
+typdef char* string;
+typdef struct string_builder{
+    char value;
+    string_builder* next;
+} string_builder;
+
+
+//function stubs
+static void signalHandler(int);
+size_t getWord(string*);
+size_t getLine(string*);
+int checkWord(string, size_t);
+size_t unravelBuilder(string_builder, string*);
+
+
+//Macros
+#define START_CLI_PARSE	{	\
+    for(int i = 0;i<argc;i++){ 	\
+	if(argv[0]=='-'&&argv[1]!='-'){	\
+	    for(size_t i = 1;argv[i]!=0){\
+		switch(argv[1]){	
+
+#define CHAR_ARG(character,action)	\
+    case character:			\
+	action				\
+	break
+
+#define END_CHAR_ARGS }}}
+#define END_CLI_ARGS
